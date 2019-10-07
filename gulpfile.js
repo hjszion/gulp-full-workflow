01ac8fb8c89e2112e51e04883117e3b8579e8168
zionHuang <hjszion@gmail.com>
2019-10-07 16:42:54 -0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -115,14 +115,6 @@ function imgMin() {
 }
 //#endregion
 
-//dev 开发相关的任务
-//1.监听sass的变化 自动编译sass
-//2.自动执行打开浏览器 启动server
-//只要.scss或.css产生变化以后 就会自动执行这个保存工作
-gulp.task("dev", function(){
-    gulp.watch(['./src/style/scss/**/*.scss', './src/style/css/**/*.css'], gulp.series(styleDev))
-});
-
 //js 任务方法
 //1.保证js开发的格式的规范性 eslint => 进行代码格式规范校验(es6 js jsx),还可以辅助我们进行格式化
 //2.es6的转码成es5 => babel 来实现
@@ -213,12 +205,16 @@ function distBrowser() {
 //2.自动执行打开浏览器 启动server
 //3.监听js的变化
 
-//dev 任务
-gulp.task('dev', gulp.series(devServer, openBrowser, function () {
-    gulp.watch(['./src/style/scss/**/*.scss', './src/style/css/**/*.css'], gulp.series(style))
-}));
+//#region dev 开发相关的任务
+//1.监听sass的变化 自动编译sass
+//2.自动执行打开浏览器 启动server
+//只要.scss或.css产生变化以后 就会自动执行这个保存工作
+gulp.task("dev", function(){
+    gulp.watch(['./src/style/scss/**/*.scss', './src/style/css/**/*.css'], gulp.series(styleDev))
+});
+//#endregion
 
-//default 任务
+//default 输入gulp的时候执行的默认任务
 //第一个参数 任务的名字 第二个参数具体要执行的任务
 gulp.task('default', gulp.series(cleanDist, gulp.parallel(js, stylePro, imgMin), revjs, copy, html, distServer, openBrowserDist));
 
